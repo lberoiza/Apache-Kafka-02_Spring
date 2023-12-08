1ae1ba93deb4de100749e9adce69c604c157868a
Luis B <luis.beroiza@adesso.de>
2023-12-08 15:41:38 +0100

    Envia mensaje a Kafka cuando la aplicacion inicia

--- src/main/java/lab/spring/kafka/KafkaSpringApplication.java
+++ src/main/java/lab/spring/kafka/KafkaSpringApplication.java
@@ -1,13 +1,23 @@
 package lab.spring.kafka;
 
+import lab.spring.kafka.services.producers.SimpleProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaSpringApplication {
+public class KafkaSpringApplication implements CommandLineRunner {
+
+  @Autowired
+  SimpleProducerService simpleProducerService;
 
   public static void main(String[] args) {
     SpringApplication.run(KafkaSpringApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    simpleProducerService.sendMessage("test-topic", "Application started");
+  }
 }
