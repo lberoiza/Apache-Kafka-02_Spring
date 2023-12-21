768cf109861c6051787d1aa9ed2a0907e6d982a1
Luis B <luis.beroiza@adesso.de>
2023-12-21 13:14:06 +0100

    Crea PersonData para ser usado como mensaje de test

---
+++ src/test/java/lab/spring/kafka/testmodels/PersonData.java
@@ -0,0 +1,47 @@
+package lab.spring.kafka.testmodels;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@Slf4j
+public class PersonData {
+
+
+
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String phoneNumber;
+  private String address;
+
+  public PersonData() {
+  }
+
+
+  public static PersonData fromJson(String json) throws JsonProcessingException {
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      return objectMapper.readValue(json, PersonData.class);
+    } catch (JsonProcessingException jsonProcessingException) {
+      log.error("Error parsing from Json: {}", jsonProcessingException.getMessage());
+      throw jsonProcessingException;
+    }
+  }
+
+  public String toJson() {
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      return objectMapper.writeValueAsString(this);
+    } catch (JsonProcessingException e) {
+      log.error("Error parsing to Json: {}", e.getMessage());
+      return "";
+    }
+  }
+
+}
